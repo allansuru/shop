bc22a78f2110e9a6ef2c6d88039504d08b09b9cd
Allan SuRu <https://github.com/allansuru/ProjetoBase.git>
2018-04-04 20:28:39 -0300

    Add components

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyBokKIYHP_ePaQY23MwbIppASoTmuEer0g",
-    authDomain: "shop-e7e3a.firebaseapp.com",
-    databaseURL: "https://shop-e7e3a.firebaseio.com",
-    projectId: "shop-e7e3a",
-    storageBucket: "",
-    messagingSenderId: "622678295368"
+    apiKey: 'AIzaSyBokKIYHP_ePaQY23MwbIppASoTmuEer0g',
+    authDomain: 'shop-e7e3a.firebaseapp.com',
+    databaseURL: 'https://shop-e7e3a.firebaseio.com',
+    projectId: 'shop-e7e3a',
+    storageBucket: '',
+    messagingSenderId: '622678295368'
   }
 };
