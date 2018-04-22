aaa7115265d744a285009b56c15ce42a13bebf45
Allan SuRu <https://github.com/allansuru/ProjetoBase.git>
2018-04-22 13:50:23 -0300

    Filtro enxugado!

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,18 +1,39 @@
 import { ProductService } from './../../product.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-admin-products',
   templateUrl: './admin-products.component.html',
   styleUrls: ['./admin-products.component.css']
 })
-export class AdminProductsComponent implements OnInit {
-  products$;
+export class AdminProductsComponent implements OnInit, OnDestroy {
+  lstProducts: any[] = [];
+  lstFiltrado: any[] = [];
+  subscription: Subscription;
 
-  constructor(private productServe: ProductService) {
-    this.products$ = this.productServe.getAll();
+  constructor(private productService: ProductService) {
   }
 
   ngOnInit() {
+    this.getProducts();
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
+  filterBySuru(query: string) {
+    this.lstFiltrado = (query) ?
+    this.lstProducts.filter(p => p['title'].toLowerCase().includes(query.toLowerCase())) :
+    this.lstProducts;
+  }
+
+  getProducts() {
+     this.subscription = this.productService.getAll().subscribe(itens => {
+        this.lstProducts = itens;
+        this.lstFiltrado = itens;
+        console.log('Produtos: ', this.lstProducts);
+      });
   }
 }
