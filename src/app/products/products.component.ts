bb31c3666b990cac3a7e2245a4b001e70b024b02
Allan SuRu <https://github.com/allansuru/ProjetoBase.git>
2018-04-26 19:39:03 -0300

    filterPoduct by Category and class.active

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,6 +1,8 @@
+import { ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../product.service';
 import { Product } from '../models/product';
+import { CategoryService } from '../category.service';
 
 @Component({
   selector: 'app-products',
@@ -10,15 +12,45 @@ import { Product } from '../models/product';
 export class ProductsComponent implements OnInit {
 
   products: Product[] = [];
+  filterProducts: Product[] = [];
+  categories: any[] = [];
+  category: string;
 
-  constructor(private productService: ProductService) { }
+  constructor(
+    private productService: ProductService,
+    private categoryService: CategoryService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit() {
     this.getAllProducts();
+    this.getAllCategories();
   }
 
   getAllProducts() {
-    this.productService.getAll().subscribe(itens => this.products = itens);
+    this.productService.getAll()
+    .subscribe(product => {
+      this.filterProducts = this.products = product;
+      this.getParamsStart();
+    });
+  }
+
+  getAllCategories() {
+    this.categoryService.getCategories()
+    .subscribe(category => this.categories = category);
+  }
+
+  filterProductsByCategory(category: string) {
+     this.filterProducts = (category) ?
+     this.products.filter(f => f.category.toLowerCase().includes(category.toLowerCase())) :
+     this.products;
+  }
+
+  getParamsStart() {
+    this.route.queryParamMap.subscribe(params => {
+      this.category = params.get('category');
+      this.filterProductsByCategory(this.category);
+});
   }
 
 }
