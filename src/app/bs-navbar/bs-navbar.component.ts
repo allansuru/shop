89d930b4e47f236f1d03a97adb28d9a249c9f36c
Allan SuRu <https://github.com/allansuru/ProjetoBase.git>
2018-05-03 19:14:26 -0300

    Add models Shopping Cart and Total Itens of quantity

--- src/app/bs-navbar/bs-navbar.component.ts
+++ src/app/bs-navbar/bs-navbar.component.ts
@@ -1,3 +1,4 @@
+import { ShoppingCart } from './../models/shopping-cart';
 import { AppUser } from './../models/app-user';
 import { AuthService } from './../auth.service';
 import { Component, OnInit } from '@angular/core';
@@ -10,15 +11,24 @@ import { ShoppingCartService } from '../shopping-cart.service';
   templateUrl: './bs-navbar.component.html',
   styleUrls: ['./bs-navbar.component.css']
 })
-export class BsNavbarComponent {
+export class BsNavbarComponent implements OnInit {
   appUser: AppUser;
+  cart$: Observable<ShoppingCart>;
 
   constructor
   (
     private auth: AuthService,
     private shoppingCartService: ShoppingCartService
-  ) {
-    auth.appUser$.subscribe(appUser => this.appUser = appUser);
+  ) {}
+
+   ngOnInit() {
+    this.auth.appUser$.subscribe(appUser => this.appUser = appUser);
+    this.getCart();
+  }
+
+  async getCart() {
+    this.cart$ = await this.shoppingCartService.getCart();
+
   }
 
   logout() {
